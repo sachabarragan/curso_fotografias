85a3cf3d90e41a9b4ee8317d0adac07eacddaaf4
Sacha Barragan <barragan.sacha@gmail.com>
2020-04-18 01:04:10 -0300

    Componentes del template separados y cargados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { GalleryComponent } from './components/pages/gallery/gallery.component';
+import { GenericComponent } from './components/pages/generic/generic.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ContactComponent } from './components/shared/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    GalleryComponent,
+    GenericComponent,
+    NavComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
